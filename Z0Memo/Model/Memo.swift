06e89be67e7ea03721686c776d7159a97184e4ca
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-12 00:34:30 +0900

    Start:Start clone coding Notepad watching youtube
    2021-10-12
    Start clone coding watching youtube
    Add navigation title(title text,Large title)
    Add Memo class (Model)

---
+++ Z0Memo/Model/Memo.swift
@@ -0,0 +1,24 @@
+//
+//  Memo.swift
+//  Z0Memo
+//
+//  Created by 재영신 on 2021/10/12.
+//
+
+import Foundation
+
+class Memo {
+    var content : String
+    var insertDate : Date
+    
+    init(content:String)
+    {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "아침은 배부르게 먹자"),
+        Memo(content: "저녁도 배부르게 먹자")
+    ]
+}
---
+++ Z0Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Z0Memo
+//
+//  Created by 재영신 on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
