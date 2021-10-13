7ea56a332d4ae07a9ef3610f4af7292eca955130
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-13 10:18:15 +0900

    Feat:add 새 메모 쓰기 화면
    메모 앱 만들기 #10

---
+++ Z0Memo/ComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ComposeViewController.swift
+//  Z0Memo
+//
+//  Created by 재영신 on 2021/10/13.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
