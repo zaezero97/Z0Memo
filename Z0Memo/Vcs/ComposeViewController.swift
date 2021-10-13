950599afe796a309873864a3e62c525bff89c598
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-13 17:49:26 +0900

    Feat:add 편집 취소 확인 기능
    메모앱 만들기 #21 - 편집 취소 확인 기능
    
    편집창에서 수정 후 기존 내용과 다르면
    pull-down시 alert창이 나타나게 되고
    저장 및 수정 취소를 선택 할 수 있는 기능을 추가

--- Z0Memo/Vcs/ComposeViewController.swift
+++ Z0Memo/Vcs/ComposeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ComposeViewController: UIViewController {
 
     var editMemo : Memo?
+    var originalText : String?
+    
     @IBOutlet weak var memoTextView: UITextView!
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil) //dismiss -> view controller가 modal로 present한 view controller를 닫는다.
@@ -45,15 +47,26 @@ class ComposeViewController: UIViewController {
         if let memo = editMemo{
             navigationItem.title = "메모 편집"
             memoTextView.text = memo.content
+            originalText = memo.content
         }else{
             navigationItem.title = "새 메모"
             memoTextView.text = " "
         }
         // DetailViewController 에서 segue를 통해 화면전환이 이루어 졌을경우는 editMemo가 nil이 아니게 된다.
         //따라서 nil이 아니면 네비게이션 아이템의 타이플을 바까주고 textview의 text를 세팅해준다.
+        
+        memoTextView.delegate = self
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.presentationController?.delegate = self
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.presentationController?.delegate = nil
+    }
     /*
     // MARK: - Navigation
 
@@ -66,7 +79,6 @@ class ComposeViewController: UIViewController {
 
 }
 
-
 extension ComposeViewController{
     static let MemoDidEdit = Notification.Name(rawValue: "MemoDidEdit")
     static let newMemoDidInsert = Notification.Name(rawValue: "new MemoDidInsert")
@@ -75,3 +87,31 @@ extension ComposeViewController{
 // 다른 view controller 에서 공용으로 접근하기 때문에 static를 사용한 거 같다.
 // static으로 선언하지 않으면 ComposeViewController 객체를 선언하고 해당 인스턴스에서 프로퍼티를 초기화해서 사용하면 인스턴스마다 Notification.Name이 달라 질 수도 있다.
 // 마지막으로 정리하자면 특정 타입의 모든 인스턴스에서 공용으로 사용될 프로퍼티를 타입 프로퍼티로 선언한다.
+
+
+
+
+extension ComposeViewController : UITextViewDelegate{
+    func textViewDidChange(_ textView: UITextView) {
+        if let original = originalText, let edited = textView.text{
+            isModalInPresentation = original != edited
+        } // text가 수정된 부분이 있으면 isModalInPresentation 값을 true로 줌으로써 pull-down으로 닫지 못하게 한다.
+    }
+}
+
+extension ComposeViewController : UIAdaptivePresentationControllerDelegate{
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) { // 사용자가 시작한 dismiss 시도가 차단되었음을 대리자에게 알립니다.
+        let alert = UIAlertController(title: "알림", message: "편집할 내용을 저장할까요?", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "저장", style: .default) { [weak self] (action) in
+            self?.save(action)
+        }
+        let cancleAction = UIAlertAction(title: "취소", style: .cancel) { [weak self] (action) in
+            self?.close(action)
+        }
+        alert.addAction(okAction)
+        alert.addAction(cancleAction)
+        
+        present(alert, animated: true, completion: nil)
+    } // iisModalInPresentation 값이 true 일 때 pull-down을 하게 되면 alert 창이 화면에 나타나게 되고 저장과 수정한 것을 취소 할 수 있다.
+}
