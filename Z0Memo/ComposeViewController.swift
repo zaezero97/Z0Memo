1cc5094bb03351023684bea32fb6e921cf25e52f
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-13 10:25:23 +0900

    Feat:add 취소 기능
    메모앱 만들기 #11 -취소 기능 구현

--- Z0Memo/ComposeViewController.swift
+++ Z0Memo/ComposeViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +24,7 @@ class ComposeViewController: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func pr epare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
